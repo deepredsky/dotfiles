bc268144ee724de69b9b915c87bc715ea54c9a11
Rajesh Sharma <rajesh@movielala.com>
2016-12-04 19:02:02 +0100

    Add custom hybrid binding for fish 2.4.0
    they removed some shared stuff like Ctrl+E

--- config.fish
+++ config.fish
@@ -87,7 +87,15 @@ function g --wraps=git
     end
 end
 
-fish_vi_key_bindings
+function hybrid_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+set -g fish_key_bindings hybrid_bindings
+
+hybrid_bindings
 
 set PATH $HOME/.rbenv/bin $PATH
 set PATH $HOME/.rbenv/shims $PATH
