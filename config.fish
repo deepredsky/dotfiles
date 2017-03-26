6adc9e2fbb78e394181a0e0ecf46f3c1b0eeecc6
Rajesh Sharma <rajesh@movielala.com>
2017-03-26 23:25:15 +0200

    Update git info in fish prompt

--- config.fish
+++ config.fish
@@ -21,18 +21,21 @@ set gray (set_color -o black)
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_showupstream 'auto'
 set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '%'
-set __fish_git_prompt_char_stagedstate '+'
-set __fish_git_prompt_char_untrackedfiles '%'
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_cleanstate '✔'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '…'
 set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
+set __fish_git_prompt_char_upstream_ahead '⇡'
+set __fish_git_prompt_char_upstream_behind '⇣'
+set ___fish_git_prompt_char_upstream_prefix ' '
+set __fish_git_prompt_showcolorhints 'yes'
 
 function fish_greeting
 end
@@ -69,14 +72,33 @@ bind \ct __fzf_ctrl_t
 bind -M insert \ct __fzf_ctrl_t
 end
 
+function fish_prompt_cwd
+  if test $PWD = $HOME
+    echo "~"
+  else
+    echo (basename $PWD)
+  end
+end
+
+function _prompt_color_for_status
+  if test $argv[1] -eq 0
+    echo green
+  else
+    echo red
+  end
+end
+
 function fish_prompt
   set last_status $status
 
   set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
+  printf '%s' (fish_prompt_cwd)
   set_color normal
 
-  printf '%s ' (__fish_git_prompt)
+  printf ' %s' (__fish_git_prompt '%s ')
+
+  set_color (_prompt_color_for_status $last_status)
+  printf "❯ "
 
   set_color normal
 end
