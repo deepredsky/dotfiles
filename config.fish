4d2f6f88f53c4431defb745f503d246d0e8ac259
Rajesh Sharma <rajesh@unity3d.com>
2017-06-14 12:33:40 +0200

    Use .bin instead of bin

--- config.fish
+++ config.fish
@@ -92,6 +92,6 @@ set -g fish_key_bindings hybrid_bindings
 set PATH $HOME/.rbenv/bin $PATH
 set PATH $HOME/.rbenv/shims $PATH
 set PATH /usr/local/sbin $PATH
-set PATH $HOME/bin $PATH
+set PATH $HOME/.bin $PATH
 rbenv rehash >/dev/null ^&1
 status --is-interactive; and source (rbenv init -|psub)
