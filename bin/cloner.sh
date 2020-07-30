1b4a837dafcbd8dc80cc098548e7476ac74d0d4e
Rajesh Sharma <rajesh@surf.net.np>
2020-07-30 18:18:23 +0200

    fix cloner script

--- bin/cloner.sh
+++ bin/cloner.sh
@@ -4,4 +4,4 @@ set -e
 
 echo $1
 
-curl -u deepredsky:$GITHUB_PR_TOKEN -s https://api.github.com/orgs/$1/repos?per_page=200 | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
+curl -u deepredsky:$GITHUB_PR_TOKEN -s "https://api.github.com/orgs/$1/repos?page=2&per_page=300" | ruby -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
