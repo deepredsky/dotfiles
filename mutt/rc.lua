f5ce2cb2e396936f7fa491e4db0e6d32eea146f9
Rajesh Sharma <rajesh.sharma@karnovgroup.com>
2021-08-22 22:11:25 +0200

    Use tmp theme file to switch neomutt theme
    ITERM_PROFILE env var is not reliably toggling when switch theme from tmux
    session

--- mutt/rc.lua
+++ mutt/rc.lua
@@ -1,5 +1,10 @@
-if os.getenv('ITERM_PROFILE') == 'Light' then
+function file_exists(name)
+   local f = io.open(name, "r")
+   return f ~= nil and io.close(f)
+end
+
+if file_exists('/tmp/light-theme') then
   mutt.call('source', '~/.mutt/mutt-colors-solarized/mutt-colors-solarized-light-16.muttrc')
 else
-  mutt.call('source', '~/.mutt/mutt-colors-solarized/mutt-colors-solarized-dark-256.muttrc')
+  mutt.call('source', '~/.mutt/mutt-colors-solarized/mutt-colors-solarized-dark-16.muttrc')
 end
