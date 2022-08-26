303658a2532e469b28a53ee307cf849a8fd51b79
Rajesh Sharma <rajesh.sharma@zendesk.com>
2022-08-26 15:20:09 +0200

    Start cloner pagination on page 1

--- bin/cloner.sh
+++ bin/cloner.sh
@@ -4,4 +4,4 @@ set -e
 
 echo $1
 
-curl -u deepredsky:$GITHUB_PR_TOKEN -s "https://api.github.com/orgs/$1/repos?page=2&per_page=300" | ruby -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
+curl -u deepredsky:$GITHUB_PR_TOKEN -s "https://api.github.com/orgs/$1/repos?page=1&per_page=300" | ruby -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
