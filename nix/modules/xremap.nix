f5b72b0f21bd046fae8cdad94cbf2fe6fc6358d2
Rajesh Sharma <rajesh@surf.net.np>
2025-10-31 20:26:28 +0100

    Add more emacs bindings

--- nix/modules/xremap.nix
+++ nix/modules/xremap.nix
@@ -21,16 +21,29 @@
       {
         name = "Emacs binding in input elements";
         remap = {
+          "C-b" = "Left";
+          "C-f" = "Right";
+          "C-p" = "Up";
+          "C-n" = "Down";
+
+          "M-b" = "Ctrl-Left";
+          "M-f" = "Ctrl-Right";
+
           "C-h" = "Backspace";
           "C-a" = "home";
           "C-e" = "end";
+          "C-k" = ["Shift-end" "Delete"];
+          "C-u" = ["Shift-home" "Delete"];
+
           "C-Shift-a" = "Shift-home";
           "C-Shift-e" = "Shift-end";
+
           "C-w" = "C-Backspace";
         };
         application.not = [
           "kitty"
           "vim"
+          "foot"
         ];
       }
     ];
